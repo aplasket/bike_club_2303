f361ab11018894a1db074b9fa1044aa57dd557e6
Ashley Plasket <ashley.plasket@gmail.com>
2023-04-25 13:38:56 -0400

    clean up comments

--- lib/biker.rb
+++ lib/biker.rb
@@ -16,12 +16,6 @@ class Biker
   end
 
   def log_ride(ride, time)
-    #The Biker can keep track of all of its previous rides and times for those rides.
-    #A Biker will not log a ride if the ride's terrain does not match their acceptable terrain. 
-    # They also won't log a ride if the ride's total distance is greater 
-    # than the Biker's max distance.
-    #IF acceptable terrain && less than max distance, keeps track of all rides and times for those rides.
-    
     ##psuedocode:
     # ?? return false /nil if not acceptable terrain || rides.total_distance < @max_distance
     # if @acceptable_terrain.include?(ride.terrain) && !@rides.keys.include?(ride) && @maxdistance, rides[ride] = [time]
@@ -35,10 +29,6 @@ class Biker
 
 
     def personal_record(ride)
-      #personal record for a specific ride. 
-      #This is the lowest time recorded for a ride. 
-      # This method will return false if the Biker hasn't completed the ride
-
       #psuedocode:
       #return false, if @rides[ride].nil? 
       # @rides[ride] min by or min ...
