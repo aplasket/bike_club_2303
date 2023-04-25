e290730226287198aba811c5b9a171e6879796ce
Ashley Plasket <ashley.plasket@gmail.com>
2023-04-25 11:45:38 -0400

    add log ride method and tests

--- lib/biker.rb
+++ lib/biker.rb
@@ -14,4 +14,24 @@ class Biker
   def learn_terrain!(terrain_type)
     @acceptable_terrain << terrain_type
   end
+
+  def log_ride(ride, time)
+    #The Biker can keep track of all of its previous rides and times for those rides.
+    #A Biker will not log a ride if the ride's terrain does not match their acceptable terrain. 
+    # They also won't log a ride if the ride's total distance is greater 
+    # than the Biker's max distance.
+    #IF acceptable terrain && less than max distance, keeps track of all rides and times for those rides.
+    
+    ##psuedocode:
+    # return false /nil if not acceptable terrain || rides.total_distance < @max_distance
+    # if @acceptable_terrain.include?(ride.terrain) && !@rides.keys.include?(ride) && @maxdistance, rides[ride] = [time]
+    #else  @acceptable_terrain.include?(ride) && @rides.keys.include?(ride), rides[ride] << time
+
+    if @acceptable_terrain.include?(ride.terrain) && @rides.keys.include?(ride) && ride.total_distance < @max_distance 
+      @rides[ride] << time
+    elsif @acceptable_terrain.include?(ride.terrain) && !@rides.keys.include?(ride) && ride.total_distance < @max_distance 
+      @rides[ride] = [time]
+    end
+
+  end
 end
--- spec/biker_spec.rb
+++ spec/biker_spec.rb
@@ -3,6 +3,14 @@ require "spec_helper"
 RSpec.describe Biker do
   before(:each) do
     @biker = Biker.new("Kenny", 30)
+    @ride1 = Ride.new({ name: "Walnut Creek Trail", 
+                        distance: 10.7, 
+                        loop: false, 
+                        terrain: :hills})
+    @ride2 = Ride.new({ name: "Town Lake", 
+                        distance: 14.9, 
+                        loop: true, 
+                        terrain: :gravel})
   end
 
   describe "#initialize" do
@@ -26,6 +34,23 @@ RSpec.describe Biker do
       @biker.learn_terrain!(:gravel)
       @biker.learn_terrain!(:hills)
       expect(@biker.acceptable_terrain).to eq([:gravel, :hills])
+    end
+  end
+
+  describe "#log ride" do
+    it "logs the ride" do
+      @biker.learn_terrain!(:gravel)
+      @biker.learn_terrain!(:hills)
+
+      @biker.log_ride(@ride1, 92.5)
+      @biker.log_ride(@ride1, 91.1)
+      @biker.log_ride(@ride2, 60.9)
+      @biker.log_ride(@ride2, 61.6)
+
+      expected = {@ride1 => [92.5, 91.1],
+                  @ride2 => [60.9, 61.6]}
+
+      expect(@biker.rides).to eq(expected)
     end
   end
 end
