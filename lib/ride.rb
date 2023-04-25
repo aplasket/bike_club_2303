bc9783a51c4478f5e642cf91003c0bc45137554f
Ashley Plasket <ashley.plasket@gmail.com>
2023-04-25 11:13:19 -0400

    add attributes, initialize tests, add loop and total distance

--- lib/ride.rb
+++ lib/ride.rb
@@ -1,3 +1,27 @@
 class Ride
+  attr_reader :name,
+              :distance,
+              :loop,
+              :terrain
 
+  def initialize(info)
+    @name = info[:name]
+    @distance = info[:distance]
+    @loop = info[:loop]
+    @terrain = info[:terrain]
+  end
+
+  def loop?
+    @loop
+  end
+
+  def total_distance
+    # if loop == true, @distance
+    #else if loop == false, @distance * 2
+    if loop?
+      @distance
+    else
+      @distance * 2
+    end
+  end
 end
